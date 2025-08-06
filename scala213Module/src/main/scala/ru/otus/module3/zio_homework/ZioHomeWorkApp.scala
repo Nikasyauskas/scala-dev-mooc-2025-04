a62f20c266d9b01f4f4ed7c55a967f33d92344a1
Nikasyauskas <nikasyauskas@gmail.com>
2025-08-07 02:08:58 +0300

    part 1

--- scala213Module/src/main/scala/ru/otus/module3/zio_homework/ZioHomeWorkApp.scala
+++ scala213Module/src/main/scala/ru/otus/module3/zio_homework/ZioHomeWorkApp.scala
@@ -1,8 +1,10 @@
 package ru.otus.module3.zio_homework
 
-import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault}
+import zio.{Scope, ZIO, ZIOAppArgs, ZIOAppDefault, Clock, ZLayer}
+import ru.otus.module3.zio_homework.appSpeedUp
 
 object ZioHomeWorkApp extends ZIOAppDefault{
-  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = ???
+  override def run = appSpeedUp.provide(ZLayer.succeed(Clock.ClockLive))
 
+  // .provide(ZLayer.succeed(Clock.ClockLive))
 }
